31b812a38387ab27d80efe668d3496c955fd9147
leekinghou <lijinhao716@163.com>
2023-12-24 16:13:24 +0800

    lc283

---
+++ golang/lc283.go
@@ -0,0 +1,12 @@
+package main
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func moveZeroes(nums []int) {
+	slowIndex := 0
+	for fastIndex := 0; fastIndex < len(nums); fastIndex++ {
+		if nums[fastIndex] != 0 {
+			nums[fastIndex], nums[slowIndex] = nums[slowIndex], nums[fastIndex]
+			slowIndex++
+		}
+	}
+}
